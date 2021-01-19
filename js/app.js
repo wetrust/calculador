23cc0f6802809b37816f1a97d03d674b56b8ba4a
cristopher castro <cristophernic@live.cl>
2021-01-19 18:33:21 -0300

    Página 2

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 import {make, the, inputDate} from './wetrust.js'
 import {fechas} from './functiones.js'
 
+var activo = "examenes"
+var paginas = ["inicio", "examenes"]
+
+
 let _fecha = new Date()
-let _fur = fechas.fur(20, _fecha)
 
 the("fexamen").value = inputDate(_fecha)
+
+let _fur = fechas.fur(20, _fecha)
 the("fur").value = inputDate(_fur)
 the("fpp").value = inputDate(fechas.fpp(_fur))
 
@@ -41,5 +46,25 @@ the("fexamen").onchange = function(){
     }
     else{
         the("eg").value = 0;
+    }
+}
+
+the("goPrelude").onclick = function(){
+    the("inicio").classList.add("d-none");
+    the("prelude").classList.remove("d-none");
+    the("examenes").classList.remove("d-none");
+    activo = "examenes"
+}
+
+the("back").onclick = function(){
+    the(activo).classList.add("d-none");
+    
+    let id = paginas.indexOf(activo)
+    activo = paginas[id-1]
+
+    the(activo).classList.remove("d-none");
+    
+    if(activo == "inicio"){
+        the("prelude").classList.add("d-none");
     }
 }
